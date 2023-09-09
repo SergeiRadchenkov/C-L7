d31b872a7b8755aaceb48069c4b1303a7e446f5e
Sergei <radchenkovsa@yandex.ru>
2023-09-10 02:27:51 +0300

    Case2_1

--- case2_1/Program.cs
+++ case2_1/Program.cs
@@ -1,3 +1,9 @@
-﻿string path = "C:\Users\Family\Desktop\C#L6\case2_1";
+﻿string path = @"C:\Users\Family\Desktop\C#L6\case2_1";
 DirectoryInfo di = new DirectoryInfo(path);
 System.Console.WriteLine(di.CreationTime);
+FileInfo[] fi = di.GetFiles();
+
+for (int i = 0; i < fi.Length; i++)
+{
+    System.Console.WriteLine(fi[i].Name);
+}
